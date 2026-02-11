a5ebe06232d5f24e1c0d7d297352692213e2c84d
andreisoprea081-ops <andreisoprea081-ops@users.noreply.github.com>
2026-02-11 09:12:16 -0500

    Run static code analysis

--- server.py
+++ server.py
@@ -1,33 +1,35 @@
+"""This module provides Web deployment of the application using Flask
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emotion_detectection_function():
+    """This code receives the text from the HTML interface and 
+    runs emotion detection over it using emotion_detector()
+    function."""
 
     text_to_analyze = request.args.get("textToAnalyze")
-
     response = emotion_detector(text_to_analyze)
 
-    if(response['dominant_emotion'] is None):
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-   
-    else:
-        return (
-        "For the given statement, the system response is: "
-        f"anger={response['anger']}, "
-        f"disgust={response['disgust']}, "
-        f"fear={response['fear']}, "
-        f"joy={response['joy']}, "
-        f"sadness={response['sadness']},"
-        f"The dominant emotion is {response['dominant_emotion']}."
-        )
-
+    return (
+    "For the given statement, the system response is: "
+    f"anger={response['anger']}, "
+    f"disgust={response['disgust']}, "
+    f"fear={response['fear']}, "
+    f"joy={response['joy']}, "
+    f"sadness={response['sadness']},"
+    f"The dominant emotion is {response['dominant_emotion']}."
+    )
 
 @app.route("/")
 def render_index_page():
-
+    """This function initiates the rendering of the main application
+    page over the Flask channel
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
